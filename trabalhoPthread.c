0f4195cc55d4c99d42d6742650ce3cf0e2daf3ae
Jamilton <52805195+JamiltonMB@users.noreply.github.com>
2021-02-21 17:25:54 -0300

    versão funcional com um pequeno bug

--- trabalhoPthread.c
+++ trabalhoPthread.c
@@ -3,6 +3,12 @@
 #include <math.h>
 #include <pthread.h>
 
+/*
+Para compilar o código use o seguinte comando no terminal: gcc trabalhoPthread.c -o run -lm -lpthread
+Para executar use argumentos em linha de comando, usando o exemplo acima uma entrada possível seria: ./run 7 200
+onde 7 é o número de threads e 200 é o número de trapézios.
+*/
+
 double areaTotal=0.00;
 int nThreads;
 
@@ -14,7 +20,7 @@ double f2(double x){
 	return sin(2.0*x)+cos(5.0*x);
 }
 
-double areaf1(int nTrapezio){
+double calcularAreaSemThreadf1(int nTrapezio){
 	double a = 0.00;
 	double b = 10.00;
 	double h = (b-a)/nTrapezio;
@@ -29,7 +35,7 @@ double areaf1(int nTrapezio){
  	return area_total;
 }
 
-double areaf2(int nTrapezio){
+double calcularAreaSemThreadf2(int nTrapezio){
 	double a = 0.00;
 	double b = 2*M_PI;
 	double h = (b-a)/nTrapezio;
@@ -39,50 +45,33 @@ double areaf2(int nTrapezio){
  		double x_i = a+i*h;
  		area_total += f2(x_i);
  	}
- 	
  	area_total = h*area_total;
  	return area_total;
 }
 
 void* calcularAreaF1(void *nTrapezio){
-	double a = 0.00;
-	double b = 10.00;
-	int n = (int)(size_t)nTrapezio;
-	double h = (b-a)/n;
-	double area_total_local = (f1(a)+f1(b))/2;
- 	
- 	for(int i=1; i<n; i++){
- 		double x_i = a+i*h;
- 		area_total_local += f1(x_i);
- 	}
- 	
- 	area_total_local = h*area_total_local;
- 	areaTotal += area_total_local;
- 	printf("%lf\n", area_total_local);
+	double area_local = calcularAreaSemThreadf1((int)(size_t)nTrapezio);
+	area_local = area_local/nThreads;
+	areaTotal += area_local;
  	pthread_exit(NULL);
 }
 
 void* calcularAreaF2(void *nTrapezio){
-	double a = 0.00;
-	double b = 2*M_PI;
-	int n = (int)(size_t)nTrapezio;
-	double h = (b-a)/n;
-	double area_total_local = (f2(a)+f2(b))/2;
-
- 	for(int i=1; i<n; i++){
- 		double x_i = a+i*h;
- 		area_total_local += f2(x_i);
- 	}
- 	
- 	area_total_local = h*area_total_local;
- 	areaTotal = areaTotal + area_total_local;
+	double area_local = calcularAreaSemThreadf2((int)(size_t)nTrapezio);
+	//area_local = area_local/nThreads;
+	areaTotal += area_local;
  	pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
 
-	int nTrapezios = 12;
-	nThreads = 10;
+	if(argc<3){
+		printf("Informe dois argumentos de linha de comando: o número de threads e número de trapézios\n");
+		return -1; 
+	}
+
+	nThreads = atoi(argv[1]);
+	int nTrapezios = atoi(argv[2]);
 
 	int vetorDeDistribuicao[nThreads];
 
@@ -117,12 +106,13 @@ int main(int argc, char *argv[]){
 	for(int i=0; i<nThreads; i++){
 		pthread_join(threads[i], &thread_return);
 	}
+	printf("Teste com f1(x): ");
 	printf("%.2e\n", areaTotal);
 
 	areaTotal = 0.00;
 	for(int i=0; i<nThreads; i++){
 		if(vetorDeDistribuicao[i] != 0){
-			status = pthread_create(&threads[i], NULL, calcularAreaF2, (void*)(size_t)vetorDeDistribuicao[i]);
+			status = pthread_create(&threads[i], NULL, calcularAreaF2, (void*)(size_t)12);
 			if(status!=0){
 				printf("Erro na criação da Thread %d\n", i);
 			}
@@ -132,11 +122,11 @@ int main(int argc, char *argv[]){
 	for(int i=0; i<nThreads; i++){
 		pthread_join(threads[i], &thread_return);
 	}
+	printf("Teste com f2(x): ");
 	printf("%.2e\n", areaTotal);
 
-
- 	printf("%.2e\n", areaf1(nTrapezios));
- 	printf("%.2e\n", areaf2(nTrapezios));
+ 	//printf("%.2e\n", calcularAreaSemThreadf1(nTrapezios));
+ 	//printf("%.2e\n", calcularAreaSemThreadf2(nTrapezios));
 
 	return 0;
 }
