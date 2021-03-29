fe46fdd4b1ed391c45cb2661f0f1c859495b860c
Jamilton <52805195+JamiltonMB@users.noreply.github.com>
2021-03-29 11:06:30 -0300

    Add files via upload

--- trabalho3-trem/trem.cpp
+++ trabalho3-trem/trem.cpp
@@ -3,7 +3,7 @@
 #include <QMutex>
 #include <iostream>
 QMutex mutex[7];
-static bool rCriticas[] = {true, true, true, true, true, true, true};
+static bool rCriticas[] = {true, true, false, true, true, true, true};
 
 //Construtor
 Trem::Trem(int ID, int x, int y){
@@ -42,14 +42,26 @@ void Trem::run(){
             }
             else if (x == 290 && y < 150){
                 rCriticas[0]=false;
-                y+=10;
-                if(y>=150){
-                    rCriticas[0]=true;
-                    mutex[0].unlock();
+                if(y >= 130){
+                    mutex[2].lock();
+                    if(rCriticas[2]){
+                        mutex[2].unlock();
+                        y+=10;
+                    }
+                }else{
+                    y+=10;
                 }
             }
             else if (x > 20 && y == 150){
-                x-=10;
+                rCriticas[0]=true;
+                mutex[0].unlock();
+                 x-=10;
+                if(x>140){
+                    rCriticas[2]=false;
+                }else{
+                    rCriticas[2]=true;
+                    mutex[2].unlock();
+                }
             }
             else
                 y-=10;
@@ -58,9 +70,32 @@ void Trem::run(){
 
         case 2: //Trem 2
             if (y == 30 && x <560){
-                x+=10;}
+                if(x >= 540){
+                     mutex[1].lock();
+                    if(rCriticas[1]){
+                        mutex[1].unlock();
+                        x+=10;
+                    }
+                }else{
+                    x+=10;
+                }
+                /*
+                if(x<=300){
+                    rCriticas[0]=false;
+                }else{
+                    rCriticas[0]=true;
+                    mutex[0].unlock();
+                }
+                */
+            }
             else if (x == 560 && y < 150){
-                y+=10;}
+                rCriticas[1]=false;
+                y+=10;
+                if(y>=150){
+                    rCriticas[1]=true;
+                    mutex[1].unlock();
+                }
+            }
             else if (x > 290 && y == 150){
                 if(x <= 310){
                      mutex[0].lock();
@@ -75,7 +110,7 @@ void Trem::run(){
             else{
                 rCriticas[0]=false;
                 y-=10;
-                if(y<=40){
+                if(y<=30){
                     rCriticas[0]=true;
                     mutex[0].unlock();
                 }
@@ -90,22 +125,52 @@ void Trem::run(){
             else if (x == 830 && y < 150)
                 y+=10;
             else if (x > 560 && y == 150){
-                x-=10;}
+                if(x <= 580){
+                     mutex[1].lock();
+                    if(rCriticas[1]){
+                        mutex[1].unlock();
+                        x-=10;
+                    }
+                }else{
+                    x-=10;
+                }
+            }
             else{
+                rCriticas[1]=false;
                 y-=10;
+                if(y<=30){
+                    rCriticas[1]=true;
+                    mutex[1].unlock();
+                }
             }
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
 
         case 4: //Trem 4
-            if (y == 150 && x <430)
+            if (y == 150 && x <430){
+                rCriticas[2]=false;
                 x+=10;
+                if(x>=300){
+                    rCriticas[2]=true;
+                    mutex[2].unlock();
+                }
+            }
             else if (x == 430 && y < 270)
                 y+=10;
-            else if (x > 160 && y == 270)
+            else if (x > 160 && y == 270){
                 x-=10;
-            else
-                y-=10;
+            }
+            else{
+                if(y <= 160){
+                     mutex[2].lock();
+                    if(rCriticas[2]){
+                        mutex[2].unlock();
+                         y-=10;
+                    }
+                }else{
+                    y-=10;
+                }
+            }
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
 
